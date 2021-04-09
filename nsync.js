4145425b29461e7fdbbe5b47aa5ba69d2a2782bf
Pj Metz <65838556+MetzinAround@users.noreply.github.com>
2021-04-09 09:07:40 -0400

    Cleaned up the code and changed to logs
    Now logging the response instead of the data
    Removed unnecessary code and white space

--- nsync.js
+++ nsync.js
@@ -1,17 +1,9 @@
 console.log("Jump in my ride...");
 
-const { Console } = require('console');
-const fs = require('fs');
-var MIM = require('mim');
-const path = require('path');
-
-
 //checking if it's in develop mode for local use
 if (process.env.NODE_ENV === "develop") {
   require("dotenv").config();
 };
-
-
 
 // Create an Twitter object to connect to Twitter API
 const Twit = require('twit');
@@ -36,44 +28,51 @@ function tweetEvent(tweet) {
   var text = tweet.text;
   var name = tweet.user.screen_name;
 
-    let regex = /(💰💰💰)/g;
-    let str = text;
+  let regex = /(💰💰💰)/g;
+  let str = text;
 
-    let partyHoppin = str.match(regex) || [];
+  let partyHoppin = str.match(regex) || [];
 
-    let feelinRight = partyHoppin.length>0;
-    
-    console.log(partyHoppin);
-    console.log(feelinRight);
+  let feelinRight = partyHoppin.length > 0;
+
+  console.log(partyHoppin);
+  console.log(feelinRight);
 
 
   if ((text.includes('@JustBotPaid') && feelinRight === true)) {
 
     const gifPath = "./justGotPaid.gif"
     // Start a reply back to the sender
-    const replyText = "@"+ name + "‍ Check the mirror, lookin' fly!";
+    const replyText = "@" + name + "‍ Check the mirror, lookin' fly!";
 
-    // Post that tweet
-    T.postMediaChunked({file_path: gifPath }, function (err, data, response) {
-        console.log(data.media_id_string)
-        const checkTheMirror = data.media_id_string;
-        const doubleCheckTheMirror = {media_id: checkTheMirror};
-          if (!err) {
-            setTimeout(() => {T.post("media/metadata/create", doubleCheckTheMirror, function(err, data, response){
-              if(!err) {
-                const moneyMoneyMoney = {status: replyText, media_ids: checkTheMirror, in_reply_to_status_id: id}
+    // postMediaChunked is a twit method for posting gifs and movies, since Twitter requires a multiple step process to post. 
+    T.postMediaChunked({ file_path: gifPath }, function (err, data, response) {
+      // two variables: pulling the media_is_string for javascript and turning it into an object
+      const checkTheMirror = data.media_id_string;
+      const doubleCheckTheMirror = { media_id: checkTheMirror };
+      if (!err) {
+        //the timeout is necessary to give time for postMediaChunked to create a media post on twitter. 
+        //it needs time to create a media Id string. 
+        setTimeout(() => {
+          T.post("media/metadata/create", doubleCheckTheMirror, function (err, data, response) {
+            if (!err) {
+              //creating an object for the T.post ('statuses/update') method
+              const moneyMoneyMoney = { status: replyText, media_ids: checkTheMirror, in_reply_to_status_id: id }
 
-                T.post('statuses/update', moneyMoneyMoney, function(err, data, response) {
-                  if (!err) {
-                    console.log(data);
-                  } else {
-                    console.log(err);
-                  }
-                });
-              }
-            } )}, 5000)
-          }
-      });
+              T.post('statuses/update', moneyMoneyMoney, function (err, data, response) {
+                if (!err) {
+                  console.log(response);
+                } else {
+                  console.log(err);
+                }
+              });
+            }
+          })
+        }, 5000)
+      } else {
+        console.log(err);
+      }
+    });
   };
 };
 
@@ -85,34 +84,40 @@ rule.hour = 5;
 rule.minute = 30;
 rule.tz = "Etc/GMT+4";
 
-function fridayNight()
-{
+function fridayNight() {
 
-const PATH ="./nsync.mp4";
-const postStatus = "Just Got Paid";
-console.log(postStatus);
+  const PATH = "./nsync.mp4";
+  const postStatus = "Just Got Paid!";
+  console.log(postStatus);
 
-T.postMediaChunked({file_path: PATH }, function (err, data, response) {
-  console.log(data)
-  const checkTheMirror = data.media_id_string;
-  if (!err) {
-    const params = {media_id: checkTheMirror}
-    setTimeout(() => { T.post('media/metadata/create', params, function (err, data, response) {
-      if (!err) { 
-        // now we can reference the media and post a tweet (media will attach to the tweet)
-        var finalParams = { status: postStatus, media_ids: [checkTheMirror] }
-  
-      T.post('statuses/update', finalParams, function (err, data, response) {
-        console.log(data)
-      })} else {
-        console.log(err);
-        } 
-      }
-    )}, 5000);
-  } else {
-    console.log(err)
+  T.postMediaChunked({ file_path: PATH }, function (err, data, response) {
+    console.log(data)
+    const checkTheMirror = data.media_id_string;
+
+    if (!err) {
+
+      const params = { media_id: checkTheMirror }
+
+      setTimeout(() => {
+        T.post('media/metadata/create', params, function (err, data, response) {
+
+          if (!err) {
+            const finalParams = { status: postStatus, media_ids: [checkTheMirror] }
+
+            T.post('statuses/update', finalParams, function (err, data, response) {
+              console.log(response)
+            })
+          } else {
+            console.log(err);
+          }
+        }
+        )
+      }, 5000);
+    } else {
+      console.log(err)
     };
-})};
+  })
+};
 
 
 const job1 = schedule.scheduleJob(rule, fridayNight);
