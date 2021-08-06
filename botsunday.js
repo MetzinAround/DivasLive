acba92757726a9aa46ae5e05d712fcf723b29e69
Pj Metz <metz.pj@gmail.com>
2021-08-06 13:50:45 +0000

    Update botsunday.js, divas.js files

--- botsunday.js
+++ botsunday.js
@@ -21,6 +21,10 @@ var config = require('./configBS.js');
 // Making a Twit object for connection to the API
 var T = new Twit(config);
 
+var stream = T.stream('statuses/filter', { track: '@TakingB_tSunday' });
+
+stream.on('tweet', cuteWithoutTheE);
+
 function greatRomances() 
 {
     const dateObj = new Date();
@@ -36,14 +40,14 @@ const job1 = schedule.scheduleJob(rule, greatRomances);
 
 let youKnowHowIDo = ["https://youtu.be/4ldjbjwim4k", "https://youtu.be/Hw7WbowK_x0", "https://youtu.be/y4AelQt5Fv0", "https://youtu.be/ap0mqwvf7H0", "https://youtu.be/CQE7gu4D1R4", "https://youtu.be/S9z3gv3MWaM", "https://youtu.be/_PBy3Lwi4Lo", "https://youtu.be/CkfOJy3TJdw", "https://youtu.be/-tyX8Ok6cQ0", "https://youtu.be/Rb1ZFBckgfM" ]
 
-let headClub = [""]
+let headClub = [" I just wanna break you down so badly...", " The weight of my decisions were impossible to hold, But they were never yours, they were never yours", " When I let you down, look past your doubt, just please, don't lose your faith in me...", " Your lipstick - his collar, Angel. I know exactly what goes on...", " Maybe I should hate you for this, never really did ever quite get that far...", " To hell with you and all your friends...", " Literate and stylish, kissable and quiet...", " I'm still not sleeping thinking I've crawled home from worse than this...", " I'm an addict for dramatics, I confuse the two for love...", " We won't stand for hazy eyes anymore..."  ]
 
 function cuteWithoutTheE() {
     var id = tweet.id_str;
     var text = tweet.text;
     var name = tweet.user.screen_name;
 
-    let regex = /(please)/gi;
+    let regex = /(emo)/gi;
     
     let timberwolves = text.match(regex) || [];
 
@@ -54,10 +58,10 @@ function cuteWithoutTheE() {
   let i = Math.floor(Math.random() * youKnowHowIDo.length);
   
   // checks text of tweet for mention of Shania Bot
-  if ((text.includes('TakingB_tSunday') && bikeScene === true)) {
+  if ((text.includes('@TakingB_tSunday') && bikeScene === true)) {
 
     // Start a reply back to the sender
-    var replyText = youKnowHowIDo[i] + "@"+ name + "  ";
+    var replyText = youKnowHowIDo[i] + "@"+ name + headClub[i]  ;
     
     // Post that tweet
     T.post('statuses/update', { status: replyText, in_reply_to_status_id: id}, tweeted);
@@ -71,7 +75,7 @@ function cuteWithoutTheE() {
       }
     }    
   } else {
-    console.log("We can't go, girls");
+    console.log("My pictures, well they don't line your mirror...");
   }
 };
     
